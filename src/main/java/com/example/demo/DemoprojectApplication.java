56abf7bc109fefa90f8dd7e030d600d7eee2e2de
ianmcnicholas <ianjmcnicholas@.com>
2022-05-19 17:19:47 +0100

    configure h2 database

--- src/main/java/com/example/demo/DemoprojectApplication.java
+++ src/main/java/com/example/demo/DemoprojectApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoprojectApplication {
 
 	public static void main(String[] args) {
+
+		ConfigurableApplicationContext configurableApplicationContext =
 		SpringApplication.run(DemoprojectApplication.class, args);
+		PersonRepository personRepository = configurableApplicationContext.getBean(PersonRepository.class);
+
+		Person myPerson = new Person("John", "Doe");
+		personRepository.save(myPerson);
+
 	}
+
 
 }
