d2b13386a643f9feaf7d3aeefe75bf57e6c0fd49
ianmcnicholas <ianjmcnicholas@.com>
2022-05-24 11:26:12 +0100

    create StudentRepository interface

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,12 +1,22 @@
 package com.example.demo.model;
 
-
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private Integer age;
--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.Id;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+//    ^^^^^ Object to work with is a student, and the id type is a Long
 
-public interface StudentRepository extends JpaRepository<Id, Long> {
+
 }
